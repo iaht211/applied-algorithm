863c647bc8cd01b76562304aa3031be2e8f85e21
iaht211 <nguyenvanthai092003@gmail.com>
2024-01-24 22:58:59 +0700

    add gold mining week4

---
+++ thuc_hanh/tuần 4/gold_mining/test2.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e6 + 1;
+
+int n, L1, L2;
+int a[N], F[N];
+
+void showq(deque<int> gq)
+{
+    deque<int> g = gq;
+    while (!g.empty()) {
+        cout << g.front() <<" ";
+        g.pop_front();
+    }
+    cout << '\n';
+}
+ 
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> L1 >> L2;
+    for(int i = 1 ; i <= n ; i++ ) cin >> a[i];
+     
+    int ans = 0;
+    deque<int> dq;
+
+    for(int i = 1 ; i <= n ; i++ ){
+        while( !dq.empty() && dq.front() < i-L2 ) dq.pop_front(); 
+        int j = i - L1;
+        if( j >= 1 ){
+            while( !dq.empty() && F[dq.back()] < F[j] ) dq.pop_back();
+            dq.push_back(j);
+        }
+        F[i] = a[i] + (dq.empty() ? 0 : F[dq.front()]);
+         showq(dq);
+        ans = max(ans, F[i]);
+    }
+
+     for( int i = 1 ; i <= n ; i++ ) cout << F[i] << " ";
+     cout <<"\n";
+    cout << ans << "\n";
+}
